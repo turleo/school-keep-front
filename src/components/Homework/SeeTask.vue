90c17d6065b22c745f7717c9bca0e509248c5237
turleo <leonid.tureyskiy@ya.ru>
2022-03-06 20:30:09 +1000

    hometask dialog is scrollable now

--- src/components/Homework/SeeTask.vue
+++ src/components/Homework/SeeTask.vue
@@ -1,34 +1,45 @@
 <template>
   <md-dialog :md-active.sync="showDialog">
-    <md-dialog-title>{{ $t('hometask.see.task') }}</md-dialog-title>
-    <div class="md-layout-item">
-      <md-field>
-        <label for="subject">{{ $t('hometask.see.subject') }}</label>
-        <md-select v-model="hometask.subject.id" name="subject" id="subject" @md-selected="save">
-          <md-option v-for="subject in subjects" v-bind:key="subject.id" :value="subject.id">{{ subject.icon }} {{ subject.title }}</md-option>
-        </md-select>
-      </md-field>
-    </div>
-    <div class="md-layout-item">
-      <md-datepicker v-model="hometask.deadline" name="deadline" @md-closed="delayedSave">
-        <label>{{ $t('hometask.see.deadline') }}</label>
-      </md-datepicker>
-    </div>
-    <div class="md-layout-item">
-      <ToDoList :tasks="hometask.tasks" :hometask-id="hometask.id"></ToDoList>
+    <div class="scrollable">
+      <md-dialog-title>{{ $t('hometask.see.task') }}</md-dialog-title>
+      <md-button @click="toggle" class="close">
+        <md-icon><svg-icon type="mdi" :path="mdiClose"></svg-icon></md-icon>
+      </md-button>
+      <div class="md-layout-item">
+        <md-field>
+          <label for="subject">{{ $t('hometask.see.subject') }}</label>
+          <md-select v-model="hometask.subject.id" name="subject" id="subject" @md-selected="save">
+            <md-option v-for="subject in subjects" v-bind:key="subject.id" :value="subject.id">{{ subject.icon }} {{ subject.title }}</md-option>
+          </md-select>
+        </md-field>
+      </div>
+      <div class="md-layout-item">
+        <md-datepicker v-model="hometask.deadline" name="deadline" @md-closed="delayedSave">
+          <label>{{ $t('hometask.see.deadline') }}</label>
+        </md-datepicker>
+      </div>
+      <div class="md-layout-item">
+        <ToDoList :tasks="hometask.tasks" :hometask-id="hometask.id"></ToDoList>
+      </div>
     </div>
   </md-dialog>
 </template>
 
 <script>
 import ToDoList from './ToDoList'
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiClose } from '@mdi/js'
 
 export default {
   name: 'SeeTask',
-  components: { ToDoList },
+  components: {
+    ToDoList,
+    SvgIcon
+  },
   data () {
     return {
-      showDialog: false
+      showDialog: false,
+      mdiClose: mdiClose
     }
   },
   methods: {
@@ -60,5 +71,14 @@ export default {
   .md-layout-item {
     margin-left: 20px;
     margin-right: 20px;
+  }
+  .scrollable {
+    overflow-y: scroll;
+    margin-top: 16px;
+  }
+  .close {
+    position: absolute;
+    right: 0;
+    min-width: 16px !important;
   }
 </style>
